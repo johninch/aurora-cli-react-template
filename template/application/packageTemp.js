ba51b09478f34ada56b075f58b6e665fb1ca817e
zhangyuchi.inch <zhangyuchi.inch@bytedance.com>
2021-01-21 20:10:48 +0800

    fix pkg.json

--- template/application/packageTemp.js
+++ template/application/packageTemp.js
@@ -3,6 +3,10 @@ module.exports = {
         start: 'node scripts/start.js',
     },
     browserslist: ['>0.2%', 'not dead', 'not op_mini all', 'ie >= 10'],
+    babel: {
+        presets: ['react-app'],
+        plugins: []
+    },
     eslintConfig: {
         extends: ['react-app', './scripts/config/eslintrc.js']
     },
